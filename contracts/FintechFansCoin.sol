66934c2065ee391cbe916e4df967806338e0a18b
Qqwy/Wiebe-Marten Wijnja <qqwy@gmx.com>
2017-09-22 23:25:26 +0200

    rewrite so solidity_flattener understands.

--- contracts/FintechFansCoin.sol
+++ contracts/FintechFansCoin.sol
@@ -4,5 +4,9 @@ import "./HumanERC20TokenInterface.sol";
    An extended version of the ERC20 Interface,
    which specifies the Token `name`, `symbol` and amount of `decimal`s.
 */
-contract FintechFansCoin is HumanERC20TokenInterface("FintechFansCoin", "FFC", 18) {
+contract FintechFansCoin is HumanERC20TokenInterface {
+    function FintechFansCoin()
+        HumanERC20TokenInterface("FintechFansCoin", "FFC", 18)
+    {
+    }
 }
