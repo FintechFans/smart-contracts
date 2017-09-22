f31080ba01f0c031d77b4bfb839d723bbf6470d3
Qqwy/Wiebe-Marten Wijnja <qqwy@gmx.com>
2017-09-22 23:24:14 +0200

    More work on the contracts.

--- contracts/FintechFansCrowdsale.sol
+++ contracts/FintechFansCrowdsale.sol
@@ -7,12 +7,16 @@ import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
 /* Restrict certain actions to only the contract's owner: */
 import 'zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
+import 'zeppelin-solidity/contracts/crowdsale/RefundableCrowdsale.sol';
 
 /* Reference to the FintechFansCoin contract, whose tokens are sold. */
-import './FintechFansCoin.sol'
+import './FintechFansCoin.sol';
 
-contract FintechFansCrowdsale is Ownable, CappedCrowdsale {
-    public uint256 tokenDecimals = 18;
+contract Pausable {
+}
+
+contract FintechFansCrowdsale is Ownable, RefundableCrowdsale, CappedCrowdsale, Pausable {
+    uint256 tokenDecimals = 18;
     /* Reference to the FintechFansCoin contract, whose tokens are sold. */
     FintechFansCoin tokenContract;
     address public foundersWallet;
@@ -20,21 +24,23 @@ contract FintechFansCrowdsale is Ownable, CappedCrowdsale {
     function FintechFansCrowdsale(
         uint256 _startTime,
         uint256 _endTime,
-        uint256 _rate, // how many token units a buyer gets per Wei
-        address _multisigWallet,
-        address _foundersWallet,
-        uint256 _cap
+        uint256 _rate,      // how many token units a buyer gets per Wei
+        address _multisigWallet, // Address FintechFans' tokens are sent to, and (when successful) Ether is sent to.
+        address _foundersWallet, // Address Founders' tokens are sent to.
+        uint256 _goal, // Minimum goal, in Wei
+        uint256 _cap   // Maximum cap, in Wei
     )
         Crowdsale(_startTime, _endTime, rate, _multisigWallet)
+        RefundableCrowdsale(_goal)
         CappedCrowdsale(5000000 * tokenDecimals * _rate)
     {
         foundersWallet = _foundersWallet;
     }
 
-
     /*
       Overridden version of Crowdsale.buyTokens because:
-      - The Wei->FFC rate depends on where we are inside the Crowdsale time range.
+      - The Wei->FFC rate depends on how many tokens have already been sold.
+      - Also mint tokens sent to FintechFans and the Founders at the same time.
      */
     function buyTokens(address beneficiary) public payable {
         require(beneficiary != 0x0);
@@ -49,25 +55,25 @@ contract FintechFansCrowdsale is Ownable, CappedCrowdsale {
         // update state
         weiRaised = weiRaised.add(weiAmount);
 
-        // Send tokens to beneficiary
+        // Mint tokens for beneficiary
         token.mint(beneficiary, tokens);
         TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
 
-        // Send tokens to FintechFans and Founders
-        uint256 fintechfans_tokens = tokens.mul(0.8);
-        uint256 founders_tokens = tokens.mul(0.2);
-        token.mint(multisigWallet, fintechfans_tokens);
-        token.mint(foundersWallet, tokens.mul(0.2));/* TODO Locked vault, split */
+        // Mint tokens for FintechFans and Founders
+        uint256 fintechfans_tokens = tokens.mul(/*0.*/8).div(10);
+        uint256 founders_tokens = tokens.mul(/*0.*/2).div(10);
+        token.mint(wallet, fintechfans_tokens);
+        token.mint(foundersWallet, founders_tokens);/* TODO Locked vault, split */
 
         forwardFunds();
   }
 
-    function currentBonusRate() public constant {
+    function currentBonusRate() public returns (uint) {
         /* TODO check how `rate' is used. */
-        if(weiRaised < (1000000 * tokenDecimals) / rate) return 1.25; // 20% discount
-        if(weiRaised < (2000000 * tokenDecimals) / rate) return 1.1764705882352942; // 15% discount
-        if(weiRaised < (4000000 * tokenDecimals) / rate) return 1.1111111111111112; // 10% discount
-        if(weiRaised < (5000000 * tokenDecimals) / rate) return 1.0526315789473684; // 5% discount
+        /* if(weiRaised < (1000000 * tokenDecimals) / rate) return 1.25; // 20% discount */
+        /* if(weiRaised < (2000000 * tokenDecimals) / rate) return 1.1764705882352942; // 15% discount */
+        /* if(weiRaised < (4000000 * tokenDecimals) / rate) return 1.1111111111111112; // 10% discount */
+        /* if(weiRaised < (5000000 * tokenDecimals) / rate) return 1.0526315789473684; // 5% discount */
         return 1; // Should never happen, as 5 million is hard cap.
     }
 }
