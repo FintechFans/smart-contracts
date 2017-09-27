9741b0231271df941fe9583f262accc3baa250e7
Qqwy/Wiebe-Marten Wijnja <qqwy@gmx.com>
2017-09-27 22:11:21 +0200

    Finish cleaning of ZenoToken tests.

--- test/ZenoToken.js
+++ test/ZenoToken.js
@@ -1,4 +1,15 @@
-var ZenoToken = artifacts.require("./ZenoToken.sol");
+'use strict';
+
+import EVMThrow from './helpers/EVMThrow';
+
+const BigNumber = web3.BigNumber;
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .use(require('chai-bignumber')(BigNumber))
+    .should();
+
+const ZenoToken = artifacts.require("./ZenoToken.sol");
 
 contract('ZenoToken', function(accounts) {
     beforeEach(async function() {
@@ -12,73 +23,40 @@ contract('ZenoToken', function(accounts) {
     it("should transfer tokens correctly", async function() {
 
         // Get initial balances of first and second account.
-        var account_one = accounts[0];
-        var account_two = accounts[1];
+        let account_one = accounts[0];
+        let account_two = accounts[1];
 
-        var account_one_starting_balance;
-        var account_two_starting_balance;
-        var account_one_ending_balance;
-        var account_two_ending_balance;
+        let amount = 100;
 
-        var amount = 10;
+        let account_one_starting_balance = await this.instance.balanceOf(account_one);
+        let account_two_starting_balance = await this.instance.balanceOf(account_two);
 
-        account_one_starting_balance = await instance.balanceOf(account_one).toNumber();
-        account_two_starting_balance = await instance.balanceOf(account_two).toNumber();
+        await this.instance.transfer(account_two, amount, {from: account_one});
 
-        await instance.transfer(account_two, amount, {from: account_one});
+        let account_one_ending_balance = await this.instance.balanceOf(account_one);
+        let account_two_ending_balance = await this.instance.balanceOf(account_two);
 
-        account_one_ending_balance = await instance.balanceOf(account_one).toNumber();
-        account_two_ending_balance = await instance.balanceOf(account_two).toNumber();
+        account_one_ending_balance.toNumber().should.be.equal(account_one_starting_balance.toNumber() - amount);
+        account_two_ending_balance.toNumber().should.be.equal(account_two_starting_balance.toNumber() + amount);
+    });
 
+    it("Should redistribute tokens correctly", async function(){
+        // Get initial balances of first and second account.
+        let account_one = accounts[0];
+        let account_two = accounts[1];
 
-        assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
-        assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
+        let amount = 5e35; // Half of available tokens (TODO: Configurable)
+
+        let account_one_starting_balance = await this.instance.balanceOf(account_one);
+        let account_two_starting_balance = await this.instance.balanceOf(account_two);
+
+        await this.instance.transfer(account_two, amount, {from: account_one});
+        await this.instance.redistribute(amount, {from: account_one});
+
+        let account_one_ending_balance = await this.instance.balanceOf(account_one);
+        let account_two_ending_balance = await this.instance.balanceOf(account_two);
+
+        assert.equal(account_two_ending_balance.toNumber(), 1e36, "Improper redistribution");
+        assert.equal(account_one_ending_balance.toNumber(), 0, "Improper redistribution");
     });
 });
-
-// contract('ZenoToken', function(accounts) {
-//     it("Should redistribute tokens correctly", function(){
-//         var instance;
-
-//         // Get initial balances of first and second account.
-//         var account_one = accounts[0];
-//         var account_two = accounts[1];
-
-//         var account_one_starting_balance;
-//         var account_two_starting_balance;
-//         var account_one_ending_balance;
-//         var account_two_ending_balance;
-
-//         var amount = 5e35;
-
-//         return ZenoToken.deployed().then(function(instance_) {
-//             instance = instance_;
-//             return instance.balanceOf.call(account_one);
-//         }).then(function(balance) {
-//             account_one_starting_balance = balance.toNumber();
-//             return instance.balanceOf.call(account_two);
-//         }).then(function(balance) {
-//             account_two_starting_balance = balance.toNumber();
-//             return instance.transfer(account_two, amount, {from: account_one});
-//         }).then(function() {
-//             return instance.redistribute(amount, {from: account_one});
-//         }).then(function(result){
-//             // console.log("Result:", result);
-//             return instance.balanceOf.call(account_one);
-//         }).then(function(balance){
-//             account_one_ending_balance = balance;
-//             return instance.balanceOf.call(account_two);
-//         }).then(function(balance){
-//             account_two_ending_balance = balance;
-
-//             // console.log(account_one_starting_balance);
-//             // console.log(account_two_starting_balance);
-//             // console.log(account_one_ending_balance.valueOf());
-//             // console.log(account_two_ending_balance.valueOf());
-
-//             assert.equal(account_two_ending_balance.valueOf(), 1e36, "Improper redistribution");
-//             assert.equal(account_one_ending_balance.valueOf(), 0, "Improper redistribution");
-//         });
-
-//     });
-// });
